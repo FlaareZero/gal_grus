45eea441b6facf0d799c1df8d13864e426684841
Mattia Rugna <60710669+FlaareZero@users.noreply.github.com>
2021-03-03 13:01:37 +0100

    Funny stuff lol

--- customize.sh
+++ customize.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 
-ui_print " Dual Speaker mod for the Xiaomi Mi 9 SE      "
-ui_print ""
+ui_print "        GAL a.k.a. GrusAudioLovers            "
+ui_print "        --------------------------            "
 ui_print "            Made by matt_zer01                "
