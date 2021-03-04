de81ea83427390257f65d5f62b2312b79f7fd3d7
Mattia Rugna <60710669+FlaareZero@users.noreply.github.com>
2021-03-04 09:57:01 +0100

    Custom logic installation
    A simple yet effective installation logic that decides whether or not your device is running a specific rom or another one!

--- customize.sh
+++ customize.sh
@@ -3,3 +3,48 @@
 ui_print "        GAL a.k.a. GrusAudioLovers            "
 ui_print "        --------------------------            "
 ui_print "            Made by matt_zer01                "
+ui_print ""
+ui_print ""
+ui_print ""
+
+#Adding a custom rom check
+#ROM Checking
+ui_print " Checking whether you're using ArrowOS or AEX... "
+#checks API
+if [ "$API" != 30 ]; then
+ ui_print "! You're not running ArrowOS / AEX! SDK Found: $API"
+ abort
+else
+ ui_print "- SDK $API : Ok!"
+ ui_print " "
+ ui_print "..."
+fi
+#checks if you're running AEX or ArrowOS
+set -x
+prop=$(getprop ro.aex.device)
+if [ "$prop" != "" ]
+then
+  $MODPATH = $MODPATH/aex 
+  ui_print "Okay! You're running AEX! Continuing installation..."
+  ui_print "Backing up old Mixer files..."
+  mkdir /sdcard/old_mixer && mv -v /vendor/etc /sdcard/old_mixer
+  ui_print "Copying Mixer files into /vendor/etc..."
+  cp -af $MODPATH /vendor/etc
+  ui_print "Setting permissions..."
+  ui_print "..."
+  ui_print "Done!"
+  exit
+else 
+  ui_print "Okay! You're running ArrowOS! Continuing installation..."
+  $MODPATH = $MODPATH/arrow 
+  ui_print "Okay! You're running ArrowOS! Continuing installation..."
+  ui_print "Backing up old Mixer files..."
+  mkdir /sdcard/old_mixer && mv -v /vendor/etc /sdcard/old_mixer
+  ui_print "Copying Mixer files into /vendor/etc..."
+  cp -af $MODPATH /vendor/etc
+  ui_print "Setting permissions..."
+  ui_print "..."
+  ui_print "Done!"
+  exit
+fi
+exit
