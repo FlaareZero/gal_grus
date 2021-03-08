2cbd8785dbbe8ad9226eb9f8e24c85938d0bca1a
Mattia Rugna <60710669+FlaareZero@users.noreply.github.com>
2021-03-08 21:37:25 +0100

    More love for the logic installation!
    Fixed backups of the mixer files (thanks Sirio!) and reworked the logic by ElPaablo! Thanks guys <3

--- customize.sh
+++ customize.sh
@@ -1,4 +1,6 @@
 #!/system/bin/sh
+#Credits for parts of the logic goes to ElPaablo (github.com/elpaablo) for the if's and under-the-hood stuff!
+# and to sirio for the idea of using cp instead of mv! Thank you guys! <3
 
 ui_print " GAL a.k.a. GrusAudioLovers  "
 ui_print " --------------------------  "
@@ -7,50 +9,64 @@ ui_print "     Made by matt_zer01      "
 ui_print " Setting up the environment..."
 
 #variables:
-mixer1 = "/vendor/etc/mixer_paths.xml"
-mixer2 = "/vendor/etc/mixer_paths_mtp.xml"
-mixer3 = "/vendor/etc/mixer_paths_i2s.xml" 
-mixer4 = "/vendor/etc/mixer_paths_skuw.xml"
+mixer1=/vendor/etc/mixer_paths.xml
+mixer2=/vendor/etc/mixer_paths_mtp.xml
+mixer3=/vendor/etc/mixer_paths_i2s.xml 
+mixer4=/vendor/etc/mixer_paths_skuw.xml
 
-#Adding a custom rom check
-#ROM Checking
-ui_print " Checking whether you're using ArrowOS or AEX... "
-#checks API
-if [ "$API" != 30 ]; then
- ui_print "! You're not running ArrowOS / AEX! SDK Found: $API"
- abort
+#checks if device is Grus
+device=$(getprop ro.product.system.device)
+if [ $device != "grus" ]; then
+  ui_print "Device not supported. Aborting installaton..."
+  abort
+fi
+
+#checks if you're running AEX, ArrowOS or SuperiorOS
+rom=""
+prop=$(getprop ro.extended.version)
+if [ "$prop" != "" ]; then
+  rom="aex"
 else
- ui_print "- SDK $API : Ok!"
- ui_print " "
- ui_print "..."
+  prop=$(getprop ro.arrow.version)
+  if [ "$prop" != "" ]; then
+    rom="arrow"
+  else
+    prop=$(getprop ro.superior.version)
+    if [ "$prop" != "" ]; then
+      rom="superior"
+    else
+      ui_print "ROM not supported. Aborting installation..."
+      abort
+    fi
+  fi
 fi
-#checks if you're running AEX or ArrowOS
-set -x
-prop=$(getprop ro.aex.device)
-if [ "$prop" != "" ]
-then
-  ui_print "Okay! You're running AEX! Continuing installation..."
-  $MODPATH = $MODPATH/aex 
-  ui_print "Backing up old Mixer files..."
-  mkdir /sdcard/old_mixer && mv -f mixer1 mixer2 mixer3 /sdcard/old_mixer
-  ui_print "Copying Mixer files into /vendor/etc..."
-  cp -af $MODPATH /vendor/etc
-  ui_print "Setting permissions..."
+
+if [ "$API" != "30" ]; then
+  ui_print "This module doesn't support API level $API. Aborting installation..."
+  exit 0
+else
+  ui_print "Device: $device - Ok!"
+  ui_print "ROM: $rom - Ok!"
+  ui_print "SDK: $API - Ok!"
+  ui_print " "
   ui_print "..."
-  ui_print "Done!"
-  exit
-else 
-  ui_print "Okay! You're running ArrowOS! Continuing installation..."
-  $MODPATH = $MODPATH/arrow 
-  ui_print "Okay! You're running ArrowOS! Continuing installation..."
-  ui_print "Backing up old Mixer files..."
-  mkdir /sdcard/old_mixer && mv -f mixer1 mixer2 mixer4 /sdcard/old_mixer
-  ui_print "Copying Mixer files into /vendor/etc..."
-  cp -af $MODPATH /vendor/etc
-  ui_print "Setting permissions..."
-  #this operation is automatically done by Magisk itself...
-  ui_print "..."
-  ui_print "Done!"
-  exit
 fi
-exit
+
+$MODPATH=$MODPATH/$rom
+bakpath=/sdcard/old_mixer
+
+ui_print "Starting installation..."
+ui_print "Backing up old Mixer files..."
+mkdir $bakpath
+cp -af $mixer1 $mixer2 $mixer3 $bakpath
+if [ $rom == "arrow" ]; then
+  cp -af $mixer4 $bakpath
+fi
+ui_print "Copying Mixer files into /vendor/etc..."
+cp -af $MODPATH /vendor/etc
+ui_print "Setting permissions..."
+#this operation is automatically done by Magisk itself...
+ui_print "..."
+ui_print "Done!"
+exit 0
+
